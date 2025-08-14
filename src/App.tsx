bf21b12d4fcfe11b70ea5304967aa9d2bb388354
[dyad] <git@dyad.sh>
2025-08-14 16:34:19 +0530

    [dyad] Resolved file conflict causing build error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense, useState, useEffect } from "react";
 import { HelmetProvider } from "react-helmet-async";
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -115,7 +114,6 @@ const App = () => {
               <FavoritesProvider>
                 <AuthProvider>
                   <TooltipProvider>
-                    <Toaster />
                     <Sonner />
                     <GlassBackground />
                     <AppContent />
