c2552a8d86f6e710e68f01e24b97328521de4a03
[dyad] <git@dyad.sh>
2025-08-15 10:12:44 +0530

    [dyad] Fixed React suspension error on mobile - wrote 1 file(s)

--- src/components/mobile/MobileLayout.tsx
+++ src/components/mobile/MobileLayout.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import BottomNavBar from './BottomNavBar';
+import { Suspense } from 'react';
+import SkeletonCard from '@/components/SkeletonCard';
 
 const MobileLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-grow pb-24">
-        <Outlet />
+        <Suspense fallback={
+          <div className="container mx-auto px-4 py-8">
+            <div className="grid grid-cols-2 gap-4">
+              {Array.from({ length: 4 }).map((_, i) => <SkeletonCard key={i} />)}
+            </div>
+          </div>
+        }>
+          <Outlet />
+        </Suspense>
       </main>
       <BottomNavBar />
     </div>
